fec997f9f0602cd55d30ef0f85630e1c6be176b8
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-04-22 00:20:59 +0800

    fix two functions and add the library

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,12 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        int length = b.size();
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                if(b.tile(i,j)==null) return true;
+            }
+        }
         return false;
     }
 
@@ -148,6 +154,13 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        int length = b.size();
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++) {
+                if(b.tile(i,j)==null) continue;
+                if(b.tile(i,j).value()==MAX_PIECE) return true;
+            }
+        }
         return false;
     }
 

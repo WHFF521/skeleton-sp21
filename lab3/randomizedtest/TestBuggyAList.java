725d60af729073c8f83399927cb998d44debefd1
AiRomance <88715229+WHFF521@users.noreply.github.com>
2024-04-24 15:48:15 +0800

    finish randomizedtest

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,45 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+        correct.addLast(4);
+        correct.addLast(5);
+        correct.addLast(6);
+        buggy.addLast(4);
+        buggy.addLast(5);
+        buggy.addLast(6);
+        assertEquals(correct.size(), buggy.size());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+    }
+    @Test
+    public void randomizedTest(){
+        AListNoResizing<Integer> correct = new AListNoResizing<>();
+        BuggyAList<Integer> buggy = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                correct.addLast(randVal);
+                buggy.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                assertEquals(correct.size(), buggy.size());
+            } else if (operationNumber == 2) {
+                if(correct.size()==0) continue;
+                // getLast
+                assertEquals(correct.getLast(), buggy.getLast());
+            } else if (operationNumber == 3) {
+                // removeLast
+                if(correct.size()==0) continue;
+                assertEquals(correct.removeLast(), buggy.removeLast());
+            }
+        }
+    }
 }
